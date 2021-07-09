c5bf35eac4c4dd038b2c19e3eebb8251374025df
Victor Ivan <viictor.ivan@gmail.com>
2021-07-09 09:22:24 -0300

    adding tests pages

--- src/index.tsx
+++ src/index.tsx
@@ -5,24 +5,27 @@ import { Global } from "./styles/global";
 import Providers from "./Providers/";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Providers>
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover={false}
-      />
-      {/* Same as */}
-      <Global />
-      <App />
+      <BrowserRouter>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover={false}
+        />
+        {/* Same as */}
+        <Global />
+        <App />
+      </BrowserRouter>
     </Providers>
   </React.StrictMode>,
   document.getElementById("root")
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,11 @@
 import { Route, Switch } from "react-router";
-
+import LoginPage from "../pages/login";
+import TestsPage from "../pages/testsPage";
 const RouterComponent = () => {
   return (
     <Switch>
-      <Route exact path="/">
-        <div>Home</div>
-      </Route>
+      <Route exact path="/" component={TestsPage} />
+      <Route exact path="/login" component={LoginPage} />
     </Switch>
   );
 };
