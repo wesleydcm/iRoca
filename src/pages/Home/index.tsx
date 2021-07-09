c2b7a046673e2afe00014c134ecec87c67b59e3c
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-07-09 16:53:50 -0300

    feat: start the "Home" page
    new file:   src/pages/Home/index.tsx
    new file:   src/pages/Home/styles.ts
    modified:   src/routes/router.tsx

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+const Home = () => {
+  return (
+    <Container>
+      <div>DTeasoidjas</div>
+    </Container>
+  );
+};
+
+export default Home;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,13 +1,15 @@
 import { Route, Switch } from "react-router";
 import LoginPage from "../pages/login";
 import TestsPage from "../pages/testsPage";
+import Home from "../pages/Home";
 const RouterComponent = () => {
-  return (
-    <Switch>
-      <Route exact path="/" component={TestsPage} />
-      <Route exact path="/login" component={LoginPage} />
-    </Switch>
-  );
+	return (
+		<Switch>
+			<Route exact path="/" component={TestsPage} />
+			<Route exact path="/login" component={LoginPage} />
+			<Route exact path="/home" component={Home} />
+		</Switch>
+	);
 };
 
 export default RouterComponent;
