5490276a42812ad0470b45586b2f99d664ba16c1
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-07-09 13:49:55 -0300

    test: update the jest test to the "ProductCardInAnnouncement" component
    modified:   src/Components/ProductCardInAnnouncement/tests/ProductCardInAnnouncement.test.tsx

--- src/Components/ProductCardInAnnouncement/tests/ProductCardInAnnouncement.test.tsx
+++ src/Components/ProductCardInAnnouncement/tests/ProductCardInAnnouncement.test.tsx
@@ -2,24 +2,8 @@ import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ProductCardInAnnouncement from "../desktop";
 import ProductCardInAnnouncementMobile from "../mobile";
-import { IEvaluation, Image, IProduct } from "../../../@types";
+import {mockedProduct} from "../../../utils/mocks";
 
-const mockedProduct: IProduct = {
-	name: "Mussum Ipsum",
-	category: "fruit",
-	description:
-		"Mussum Ipsum, cacilds vidis litro abertis. Per aumento de cachacis, eu reclamis.",
-	userId: 1,
-	price: 2.5,
-	isOrganic: true,
-	qty: 100,
-	images: [
-		{
-			url: "http://2.bp.blogspot.com/-M1-bXKeN3Ww/TfvOP9orTpI/AAAAAAAABqg/VdN6nIOw__8/s1600/morango.png",
-		} as Image,
-	],
-	evaluations: [{} as IEvaluation],
-};
 
 describe('Component "ProductCardInCart":', () => {
 	const title = "Should this component";
