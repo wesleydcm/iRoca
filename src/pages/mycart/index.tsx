36c76de6e02d3502d1fff035f91b9b5ace1fd323
Marcelo Romão <mcabralromao@gmail.com>
2021-07-09 19:08:15 -0300

    feat: mycart page

--- src/pages/mycart/index.tsx
+++ src/pages/mycart/index.tsx
@@ -2,20 +2,39 @@ import { WINDOW_SIZE_DESKTOP } from "../../utils/index";
 import { useWindow } from "../../Providers/window";
 import MenuMobile from "../../Components/Menu/mobile";
 import MenuDesktop from "../../Components/Menu/desktop";
+import { useUser } from "../../Providers/user";
 
-const MyCart = () => {
+const MyCart = (): JSX.Element => {
+
+    const {user, initController, setUser} = useUser();
 
     const { pageWidth } = useWindow();
 
     if (pageWidth < WINDOW_SIZE_DESKTOP) {
         return (
-       
-            <MenuMobile></MenuMobile>
+            <>
+            <h1>Carrinho</h1>
+            {user.auth === true ? (""): (
+                <>
+                <p>Seu carrinho está vazio. Que tal irmos às compras?</p>
+                <span>Deseja voltar aos anúncios? Só clicar aqui.</span>
+                </>
+            )}
+            <MenuMobile />
+            </>
         )
     } else {
         return (
-       
-            <MenuDesktop></MenuDesktop>
+            <>
+            <h1>Carrinho</h1>
+            {user.auth === true ? (""): (
+                <>
+                <p>Seu carrinho está vazio. Que tal irmos às compras?</p>
+                <span>Deseja voltar aos anúncios? Só clicar aqui.</span>
+                </>
+            )}
+            <MenuDesktop />
+            </>
         )
     }
     
