ea1de39f88c22f7c994e88f672b6eff72739892a
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-07-09 20:04:32 -0300

    feat: improving the "Home" page
    modified:   src/Components/Button/index.tsx
    modified:   src/Components/Loading/styles.ts
    modified:   src/pages/Home/index.tsx
    modified:   src/pages/Home/styles.ts
    modified:   src/routes/router.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,18 @@
-import { Container } from "./styles";
-import Loading from "../../Components/Loading";
+import { Wrapper } from "./styles";
+import { ReactComponent as LogoSvg } from "../../assets/images-mobile/logo.svg";
+import { ReactComponent as SearchSvg } from "../../assets/images-desktop/search.svg";
+import InputIconMobile from "../../Components/InputIcon/mobile";
+import { useState } from "react";
 
-const Home = () => {
+const HomeMobile = () => {
+	const [searchValue, setSearchValue] = useState<string>("");
+
 	return (
-		<Container>
-		</Container>
+		<Wrapper>
+			<LogoSvg data-css="logo" />
+			<InputIconMobile action="search" value={searchValue} setValue={setSearchValue} icon={SearchSvg}/>
+		</Wrapper>
 	);
 };
 
-export default Home;
+export default HomeMobile;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,14 +2,14 @@ import { Route, Switch } from "react-router";
 import LoginPage from "../pages/login";
 // import MyCart from "../pages/mycart";
 import TestsPage from "../pages/testsPage";
-import Home from "../pages/Home";
+import HomeMobile from "../pages/Home";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={TestsPage} />
       <Route exact path="/login" component={LoginPage} />
-			<Route exact path="/home" component={Home} />
+			<Route exact path="/home" component={HomeMobile} />
       {/* <Route path="/mycart" component={MyCart} /> */}
     </Switch>
   );
