8bc87417fa9b4a5117c80a8a4d1e2e37b597db71
Victor Ivan <viictor.ivan@gmail.com>
2021-07-10 20:26:28 -0300

    feat: login desktop

--- src/Providers/window/index.tsx
+++ src/Providers/window/index.tsx
@@ -1,29 +1,37 @@
-import { createContext, useContext, useEffect, useState, ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
 import { PageWidth } from "../../@types";
 
 interface Props {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const WindowCTX = createContext({} as PageWidth);
 
 export const WindowProvider = ({ children }: Props) => {
+  const [pageWidth, setPageWidth] = useState<number>(window.innerWidth);
 
-	const [pageWidth, setPageWidth] = useState<number>(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener("resize", () => setPageWidth(window.innerWidth));
+  }, []);
+  useEffect(() => {
+    console.log(pageWidth);
+  }, [pageWidth]);
 
-	useEffect(() => {
-		window.addEventListener("resize", () => setPageWidth(window.innerWidth));
-	}, []);
-
-	return (
-		<WindowCTX.Provider
-			value={{
-				pageWidth,
-			}}
-		>
-			{children}
-		</WindowCTX.Provider>
-	);
+  return (
+    <WindowCTX.Provider
+      value={{
+        pageWidth,
+      }}
+    >
+      {children}
+    </WindowCTX.Provider>
+  );
 };
 
 export const useWindow = () => useContext(WindowCTX);
