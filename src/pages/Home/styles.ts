86335e4a5d1e40d0b7f57659af97af03859dcb25
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-07-10 03:06:21 -0300

    style: implement and style the "Home" page mobile version
    Changes to be committed:
    modified:   src/Components/ProductCardInAnnouncement/mobile/index.tsx
    modified:   src/Components/ProductCardInAnnouncement/styles.ts
    new file:   src/pages/Home/desktop/index.tsx
    deleted:    src/pages/Home/index.tsx
    new file:   src/pages/Home/mobile/index.tsx
    modified:   src/pages/Home/styles.ts
    modified:   src/utils/index.ts
    modified:   src/utils/mocks.ts

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,17 @@
 import styled from "styled-components";
+import { WINDOW_SIZE_DESKTOP } from "../../utils";
 
 export const Wrapper = styled.div`
+	width: 100%;
+	height: fit-content;
+	margin-inline: auto;
+	padding: 20px;
+	padding-bottom: 120px;
+	font-family: var(--poppins);
+
 	svg[data-css="logo"] {
+		width: 45px;
+		height: 45px;
 		path {
 			stroke: var(--green400);
 		}
@@ -9,5 +19,59 @@ export const Wrapper = styled.div`
 		path:nth-child(4) {
 			fill: var(--green400);
 		}
+	}
+
+	ul[data-css="filtersWrapper"],
+	ul[data-css="filtersWrapper"] > li {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	ul[data-css="filtersWrapper"] {
+		flex-wrap: wrap;
+		margin-top: 15px;
+	}
+
+	ul[data-css="filtersWrapper"] > li {
+		flex-direction: column;
+		margin-top: 15px;
+	}
+
+	ul[data-css="filtersWrapper"] > li > button {
+		border: none;
+	}
+
+	> h3,
+	ul[data-css="filtersWrapper"] > li > span {
+		text-transform: capitalize;
+		font-weight: 600;
+		color: var(--gray700);
+	}
+
+	ul[data-css="filtersWrapper"] > li > span {
+		font-size: 0.75rem;
+	}
+
+	ul[data-css="filtersWrapper"] > li > button > img {
+		width: 90px;
+		height: 90px;
+		border-radius: 50%;
+	}
+	ul[data-css="filtersWrapper"] > li:nth-child(3) > button > img {
+		border-radius: unset;
+	}
+	> h3 {
+		font-size: 1.8rem;
+		margin: 30px 0;
+	}
+
+	ul[data-css="productsWrapper"] {
+		li + li {
+			margin-top: 15px;
+		}
+	}
+
+	@media only screen and (min-width: ${`${WINDOW_SIZE_DESKTOP}px`}) {
 	}
 `;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,21 +6,30 @@ export const USER_LOCALSTORAGE_FLAG = "@iroca/user";
 export const PRODUCTS_LOCALSTORAGE_FLAG = "@iroca/products";
 export const CART_LOCALSTORAGE_FLAG = "@iroca/cart";
 
+export const categoriesAndTypes = {
+	FRUIT: "frutas",
+	VEGETABLES1: "legumes",
+	VEGETABLES2: "verduras",
+	FAVORITES: "favoritos",
+	ORGANICS: "orgânicos",
+	COMMONS: "comuns",
+};
+
 export const successToast = (message: string) => {
-  toast(message, {
-    position: "top-right",
-    type: "success",
-  });
+	toast(message, {
+		position: "top-right",
+		type: "success",
+	});
 };
 
 export const errorToast = (message: string) => {
-  toast(message, {
-    position: "top-right",
-    type: "error",
-  });
+	toast(message, {
+		position: "top-right",
+		type: "error",
+	});
 };
 export const priceFormatter = (value: number | bigint) =>
-  Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  }).format(value);
+	Intl.NumberFormat("pt-BR", {
+		style: "currency",
+		currency: "BRL",
+	}).format(value);
