1188d238846e20fab111f56a229692ba33ca346b
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-07-10 03:05:02 -0300

    feat: apply the Menu component bofore the Routes switch
    Changes to be committed:
    modified:   src/Components/Menu/mobile/styles.ts
    modified:   src/routes/router.tsx

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,17 +2,26 @@ import { Route, Switch } from "react-router";
 import LoginPage from "../pages/login";
 // import MyCart from "../pages/mycart";
 import TestsPage from "../pages/testsPage";
-import HomeMobile from "../pages/Home";
+import HomeMobile from "../pages/Home/mobile";
+import MenuMobile from "../Components/Menu/mobile";
+import MenuDesktop from "../Components/Menu/desktop";
+import { useWindow } from "../Providers/window";
 
 const Routes = () => {
-  return (
-    <Switch>
-      <Route exact path="/" component={TestsPage} />
-      <Route exact path="/login" component={LoginPage} />
-			<Route exact path="/home" component={HomeMobile} />
-      {/* <Route path="/mycart" component={MyCart} /> */}
-    </Switch>
-  );
+	const { pageWidth } = useWindow();
+
+	return (
+		<>
+			{pageWidth > 899 && <MenuDesktop />}
+			<Switch>
+				<Route exact path="/" component={TestsPage} />
+				<Route exact path="/login" component={LoginPage} />
+				<Route exact path="/home" component={HomeMobile} />
+				{/* <Route path="/mycart" component={MyCart} /> */}
+			</Switch>
+			{pageWidth < 900 && <MenuMobile />}
+		</>
+	);
 };
 
 export default Routes;
