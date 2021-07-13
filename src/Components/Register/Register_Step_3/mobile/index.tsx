13ed3c73fe7975043e87164977a177562bf46090
Felipe <adfelipealmeida@gmail.com>
2021-07-13 11:23:45 -0300

    feat: complete the address fields by typping the entire zip code

--- src/Components/Register/Register_Step_3/mobile/index.tsx
+++ src/Components/Register/Register_Step_3/mobile/index.tsx
@@ -8,7 +8,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IAddress } from "../../../../@types";
 
 interface FormValues {
@@ -83,6 +83,14 @@ const RegisterStep2Mobile = () => {
       setStreetInput(response.data.logradouro);
     });
   };
+
+  useEffect(() => {
+    if (cepValue) {
+      if (cepValue.length) {
+        handleClick();
+      }
+    }
+  }, [cepValue]);
 
   return (
     <Container>
