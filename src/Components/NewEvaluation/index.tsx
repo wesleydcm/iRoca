13828baa6fe522b5f37ee1d213669f77c5a45e16
Heitor <heitorrezendeborba@gmail.com>
2021-07-14 12:20:06 -0300

    fix: product rating

--- src/Components/NewEvaluation/index.tsx
+++ src/Components/NewEvaluation/index.tsx
@@ -1,15 +1,53 @@
 import RatingStars from "../RatingStars";
 import { Dispatch, SetStateAction } from "react";
 import Button from "../../Components/Button";
-import { Container, CloseButton, SkipButton, BlurBackground } from "./styles";
+import {
+  Container,
+  CloseButton,
+  SkipButton,
+  BlurBackground,
+  ContainerTeste,
+} from "./styles";
 import { useState } from "react";
 
 interface NewEvaluationProps {
   isOpened: boolean;
   setIsOpened: Dispatch<SetStateAction<boolean>>;
   evaluationTarget: "product" | "producer";
-  handleSubmit: () => void;
+  handleSubmit: (feedback: string) => void;
 }
+
+const Motion = {
+  hidden: {
+    x: +100,
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "tween",
+      delay: 0.2,
+      duration: 1,
+    },
+  },
+};
+
+const Motion2 = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "tween",
+      delay: 0.2,
+      duration: 1,
+    },
+  },
+};
 
 const NewEvaluation = ({
   isOpened,
@@ -17,36 +55,79 @@ const NewEvaluation = ({
   evaluationTarget,
   handleSubmit,
 }: NewEvaluationProps) => {
-  const [feedback, setFeedback] = useState<string>("");
-  const [value, setValue] = useState<number>(0);
+  const [feedbackProduct, setFeedbackProduct] = useState<string>("");
+  const [productRating, setProductRating] = useState<number>(0);
+  const [feedbackProducer, setFeedbackProducer] = useState<string>("");
+  const [producerRating, setProducerRating] = useState<number>(0);
+  const [firstEvaluation, setFirstEvaluation] = useState<boolean>(false);
 
   return (
     <>
       {isOpened && (
         <BlurBackground>
-          <section>
-            <Container>
-              {evaluationTarget === "product" ? (
+          {!firstEvaluation ? (
+            <section>
+              <Container variants={Motion} initial="hidden" animate="visible">
                 <h2>Como foi a qualidade dos produtos?</h2>
-              ) : (
+
+                <CloseButton onClick={() => setIsOpened(false)}>X</CloseButton>
+
+                <RatingStars
+                  readOnly={false}
+                  value={productRating}
+                  setValue={setProductRating}
+                ></RatingStars>
+
+                <h2>Se quiser, podes dar um feedback:</h2>
+                <input
+                  onChange={(e) => setFeedbackProduct(e.target.value)}
+                ></input>
+                <Button
+                  color={"green"}
+                  onClick={() => setFirstEvaluation(true)}
+                  disabled={!productRating}
+                >
+                  Enviar avaliação
+                </Button>
+                <SkipButton onClick={() => setFirstEvaluation(true)}>
+                  Pular
+                </SkipButton>
+              </Container>
+            </section>
+          ) : (
+            <section>
+              <ContainerTeste
+                variants={Motion2}
+                initial="hidden"
+                animate="visible"
+              >
                 <h2>Como você avalia o produtor?</h2>
-              )}
 
-              <CloseButton onClick={() => setIsOpened(false)}>X</CloseButton>
+                <CloseButton onClick={() => setIsOpened(false)}>X</CloseButton>
 
-              <RatingStars
-                readOnly={false}
-                value={value}
-                setValue={setValue}
-              ></RatingStars>
-              <h2>Se quiser também pode dar um feedback!</h2>
-              <input onChange={(e) => setFeedback(e.target.value)}></input>
-              <Button color={"green"} onClick={() => handleSubmit()}>
-                Enviar avaliação
-              </Button>
-              <SkipButton onClick={() => setIsOpened(false)}>Pular</SkipButton>
-            </Container>
-          </section>
+                <RatingStars
+                  readOnly={false}
+                  value={producerRating}
+                  setValue={setProducerRating}
+                ></RatingStars>
+
+                <h2>Se quiser, pode dar um feedback:</h2>
+                <input
+                  onChange={(e) => setFeedbackProducer(e.target.value)}
+                ></input>
+                <Button
+                  color={"green"}
+                  onClick={() => handleSubmit(feedbackProducer)}
+                  disabled={!producerRating}
+                >
+                  Enviar avaliação
+                </Button>
+                <SkipButton onClick={() => setIsOpened(false)}>
+                  Pular
+                </SkipButton>
+              </ContainerTeste>
+            </section>
+          )}
         </BlurBackground>
       )}
     </>
--- src/Components/NewEvaluation/styles.ts
+++ src/Components/NewEvaluation/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { motion } from "framer-motion";
 export const BlurBackground = styled.div`
 position: fixed;
   z-index: 3000;
@@ -8,14 +8,11 @@ position: fixed;
   background: rgba(0, 0, 0, 0.8);
   top: 0;
   left: 0;
-  section {
-
-    
+  section {    
   position: absolute;
   z-index: 3001;
   
-  
-  border-radius: 15px;
+    border-radius: 15px;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
@@ -51,7 +48,7 @@ export const SkipButton = styled.button`
   }
 `;
 
-export const Container = styled.div`
+export const Container = styled(motion.div)`
   width: 361px;
   height: 323px;
   background-color: #ffffff;
@@ -86,5 +83,49 @@ export const Container = styled.div`
     font-size: 13px;
     color: var(--gray600);
     line-height: 19.5px;
+  }
+`;
+
+export const ContainerTeste = styled(motion.div)`
+  width: 361px;
+  height: 323px;
+  background-color: #ffffff;
+  transition: all 0.2s;
+  transform: translateX(-100px);
+  position: relative;
+  box-shadow: 0 2px 5px 2px var(--shadow-black);
+  margin-inline: auto;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  flex-wrap: wrap;
+  overflow: hidden;
+  padding: 30px;
+
+  @media only screen and (max-width: 400px) {
+    width: 100%;
+    height: 323px;
+  }
+
+  input {
+    width: 306px;
+    height: 97px;
+    border: 3px solid var(--green400);
+    border-radius: 5px;
+  }
+
+  h2,
+  h4 {
+    font-weight: 500;
+    font-family: var(--poppins);
+    font-size: 13px;
+    color: var(--gray600);
+    line-height: 19.5px;
+  }
+  h4 {
+    display: inline-block;
+    color: var(--green300);
   }
 `;
