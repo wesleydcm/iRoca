e45a18814ebca510e3671a3916f63a54c49716bd
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-07-15 20:56:31 -0300

    fix: pre pull

--- src/pages/productPage/desktop/index.tsx
+++ src/pages/productPage/desktop/index.tsx
@@ -15,210 +15,196 @@ import { ReactComponent as HeartSVG } from "../../../assets/images-mobile/heart.
 import { errorToast, successToast } from "../../../utils";
 
 interface Params {
-  id: string;
+	id: string;
 }
 const ProductPageComponentDesktop = () => {
-  const imageURL =
-    "https://i.pinimg.com/736x/06/7f/fc/067ffce23a51a61b247f86e8b7936ee0.jpg";
-  const imageURL2 =
-    "https://image.freepik.com/vector-gratis/mercado-linea-caja-carton-frutas-verduras-entrega-alimentos-frescos-tienda-comestibles_24640-64229.jpg";
+	const [product, setProducts] = useState<IProduct>({} as IProduct);
+	const [average, setAverage] = useState<number>(0);
+	const param: Params = useParams();
+	const { initController, user } = useUser();
+	const [qty, setQty] = useState<number>(0);
+	const [total, setTotal] = useState<number>(0);
+	const { cart, setCart } = useCart();
 
-  const defaultEvaluation = {
-    name: "Anonymus",
-    image: imageURL2,
-    feedback: ``,
-    grade: 2,
-  };
+	const controller = initController();
 
-  const [product, setProducts] = useState<IProduct>({} as IProduct);
-  const [average, setAverage] = useState<number>(0);
+	const price = product?.price || 0;
 
-  const param: Params = useParams();
-  const { initController, user } = useUser();
-  const [qty, setQty] = useState<number>(0);
-  const [total, setTotal] = useState<number>(0);
-  const { cart, setCart } = useCart();
+	useEffect(() => {
+		const getProductData = async () => {
+			const productData = await controller.getProduct(Number(param.id));
+			const Average = await controller.getEvaluationsAverage(productData);
+			setAverage(Average.average);
 
-  const controller = initController();
+			const newEvaluations = await controller.getAllEvaluationsData(
+				productData.evaluations,
+			);
+			productData.evaluations = newEvaluations;
+			setProducts(productData);
+		};
 
-  const price = product?.price || 0;
+		getProductData();
+		// eslint-disable-next-line
+	}, []);
 
-  useEffect(() => {
-    const getProductData = async () => {
-    
-      const productData = await controller.getProduct(Number(param.id));
-      const Average = await controller.getEvaluationsAverage(productData);
-      setAverage(Average.average);
+	const increment = () => {
+		setQty(qty + 10);
+	};
+	const decrement = () => {
+		if (qty >= 10) {
+			setQty(qty - 10);
+		}
+	};
 
-      const newEvaluations = await controller.getAllEvaluationsData(
-        productData.evaluations
-      );
-      productData.evaluations = newEvaluations;
-      setProducts(productData);
-    };
+	const addFavorites = () => {
+		const { favorites } = user.personalData;
 
-    getProductData();
-    // eslint-disable-next-line
-  }, []);
+		const favoriteProduct = {
+			id: user.personalData.id,
+			personalData: {
+				favorites: [...favorites, product.id],
+			},
+			token: user.token,
+		};
+		controller.handleFavorite(favoriteProduct);
+	};
 
-  const increment = () => {
-    setQty(qty + 10);
-  };
-  const decrement = () => {
-    if (qty >= 10) {
-      setQty(qty - 10);
-    }
-  };
+	const addToCart = () => {
+		const newProduct = { product: { ...product, qty }, totalPrice: total };
+		if (newProduct.product.qty > 0) {
+			if (cart.length > 0) {
+				if (newProduct.product.userId === cart[0].product.userId) {
+					const haveProductInCart = cart.filter(
+						item => item.product.id === newProduct.product.id,
+					);
+					if (haveProductInCart.length > 0) {
+						const newProduct2 = {
+							totalPrice:
+								haveProductInCart[0].totalPrice + newProduct.totalPrice,
+							product: {
+								...haveProductInCart[0].product,
+								qty: haveProductInCart[0].product.qty + newProduct.product.qty,
+							},
+						};
+						const newCart = cart.map(item => {
+							if (item.product.id === newProduct2.product.id) {
+								return newProduct2;
+							} else {
+								return item;
+							}
+						});
+						setCart(newCart);
+						successToast("O produto foi adicionado ao carrinho");
+					} else {
+						setCart([
+							...cart,
+							{
+								product: {
+									...newProduct.product,
+								},
+								totalPrice: newProduct.totalPrice,
+							},
+						]);
+						successToast("O produto foi adicionado ao carrinho");
+					}
+				} else {
+					errorToast(
+						"Não é possível colocar no carrinho produtos de produtores diferentes",
+					);
+				}
+			} else {
+				setCart([
+					...cart,
+					{
+						product: {
+							...newProduct.product,
+						},
+						totalPrice: newProduct.totalPrice,
+					},
+				]);
+				successToast("O produto foi adicionado ao carrinho");
+			}
+		} else {
+			errorToast("Informe alguma quantidade");
+		}
+	};
 
-  const addFavorites = () => {
+	useEffect(() => {
+		setTotal(price * qty);
+		// eslint-disable-next-line
+	}, [qty]);
+console.log('product.evaluations :>> ', product.evaluations);
+	return (
+		<>
+			<Menu />
 
-    const {favorites} = user.personalData
-    
-    const favoriteProduct = {
-      id:user.personalData.id,
-      personalData:{
-        favorites:[...favorites, product.id]
-      },
-      token: user.token
-    }
-    controller.handleFavorite(favoriteProduct);
-  }
+			<Total>
+				<div className="favorite">
+					{user !== null &&
+						user.auth &&
+						!user.personalData.favorites.includes(product.id) && (
+							<button onClick={addFavorites}>
+								Classificar como favorito
+								<HeartSVG />
+							</button>
+						)}
+				</div>
+				<div className="totalButtons">
+					<span className="total">{priceFormatter(total)}</span>
+					<div className="buttons">
+						<button onClick={decrement}>-</button>
+						<span>{qty}Kg</span>
+						<button onClick={increment}>+</button>
+					</div>
+				</div>
+			</Total>
 
-  const addToCart = () => {
-    const newProduct = { product: { ...product, qty }, totalPrice: total };
-    if (newProduct.product.qty > 0) {
-      if (cart.length > 0) {
-        if (newProduct.product.userId === cart[0].product.userId) {
-          const haveProductInCart = cart.filter(
-            (item) => item.product.id === newProduct.product.id
-          );
-          if (haveProductInCart.length > 0) {
-            const newProduct2 = {
-              totalPrice:
-                haveProductInCart[0].totalPrice + newProduct.totalPrice,
-              product: {
-                ...haveProductInCart[0].product,
-                qty: haveProductInCart[0].product.qty + newProduct.product.qty,
-              },
-            };
-            const newCart = cart.map((item) => {
-              if (item.product.id === newProduct2.product.id) {
-                return newProduct2;
-              } else {
-                return item;
-              }
-            });
-            setCart(newCart);
-            successToast("O produto foi adicionado ao carrinho")
-          } else {
-            setCart([
-              ...cart,
-              {
-                product: {
-                  ...newProduct.product,
-                },
-                totalPrice: newProduct.totalPrice,
-              },
-            ]);
-            successToast("O produto foi adicionado ao carrinho")
-          }
-        } else {
-          errorToast("Não é possível colocar no carrinho produtos de produtores diferentes")
-        }
-      } else {
-        setCart([
-          ...cart,
-          {
-            product: {
-              ...newProduct.product,
-            },
-            totalPrice: newProduct.totalPrice,
-          },
-        ]);
-        successToast("O produto foi adicionado ao carrinho")
-      }
-    } else {
-    errorToast("Informe alguma quantidade")
-    }
-  };
+			<Container>
+				<h1>{product?.name}</h1>
+				<div className="container">
+					<Carousel itemsToShow={1} isRTL={false} showArrows={true}>
+						{product &&
+							product.images &&
+							product.images.map((image, index) => (
+								<img src={`${image.url}`} alt={product.name} key={index} />
+							))}
+					</Carousel>
+					<ProducerCard producerId={product.userId} average={average} />
+				</div>
+				<Button type="button" color="green" onClick={addToCart}>
+					Adicionar ao carrinho
+				</Button>
+				<div className="scroll">
+					<div className="qty">
+						Em estoque <span>{product?.qty}kg</span>
+					</div>
+					<p>{product?.description}</p>
 
-  useEffect(() => {
-    setTotal(price * qty);
-    // eslint-disable-next-line
-  }, [qty]);
+					<div className="general-evaluation">
+						<h3>Avaliações</h3>
+						<div>
+							<Stars readOnly={true} value={average} />
+							<span>Avaliação geral</span>
+						</div>
+					</div>
 
-  return (
-    <>
-      <Menu />
-
-      <Total>
-        <div className="favorite">
-          {(user !== null && user.auth) &&
-            (!user.personalData.favorites.includes(product.id) &&
-            <button onClick={addFavorites}>Classificar como favorito<HeartSVG/></button>
-            )
-          }
-        </div>
-        <div className = "totalButtons">
-          <span className="total">{priceFormatter(total)}</span>
-          <div className="buttons">
-            <button onClick={decrement}>-</button>
-            <span>{qty}Kg</span>
-            <button onClick={increment}>+</button>
-          </div>
-        </div>
-      </Total>
-     
-      <Container>
-        <h1>{product?.name}</h1>
-        <div className="container">
-          <Carousel itemsToShow={1} isRTL={false} showArrows={true}>
-            {!!product.images ? (
-              product.images.map((obj, index) => {
-                return (
-                  <img src={`${obj.url}`} alt={product.name} key={index} />
-                );
-              })
-            ) : (
-              <img src={`${imageURL2}`} alt={product.name} />
-            )}
-          </Carousel>
-          <ProducerCard producerId={product.userId} average={average} />
-        </div>
-        <Button type="button" color="green" onClick={addToCart}>
-          Adicionar ao carrinho
-        </Button>
-        <div className="scroll">
-          <div className="qty">
-            Em estoque <span>{product?.qty}kg</span>
-          </div>
-          <p>{product?.description}</p>
-
-          <div className="general-evaluation">
-            <h3>Avaliações</h3>
-            <div>
-              <Stars readOnly={true} value={average} />
-              <span>Avaliação geral</span>
-            </div>
-          </div>
-
-          <div className="evaluation-cards">
-            {!!product &&
-              !!product.evaluations &&
-              product.evaluations.map((item, index) => {
-                return (
-                  <EvaluationCard
-                    scenery="desktop"
-                    evaluation={item || defaultEvaluation}
-                    key={index}
-                  />
-                );
-              })}
-          </div>
-        </div>
-      </Container>
-    </>
-  );
+					<div className="evaluation-cards">
+						{product &&
+							product.evaluations &&
+							product.evaluations.map((item, index) => {
+								return (
+									<EvaluationCard
+										scenery="desktop"
+										evaluation={item}
+										key={index}
+									/>
+								);
+							})}
+					</div>
+				</div>
+			</Container>
+		</>
+	);
 };
 
 export default ProductPageComponentDesktop;
