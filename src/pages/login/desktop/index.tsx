8d8ce9a8e764f1460aa4df7d206467e80967b539
Felipe <adfelipealmeida@gmail.com>
2021-07-15 11:28:02 -0300

    feat: fix login desktop

--- src/pages/login/desktop/index.tsx
+++ src/pages/login/desktop/index.tsx
@@ -10,74 +10,76 @@ import { Link, useHistory } from "react-router-dom";
 import { ReactComponent as LogoSVGdesktop } from "../../../assets/images-desktop/logo.svg";
 
 const Motion = {
-	hidden: {
-		opacity: 0,
-	},
-	visible: {
-		opacity: 1,
-		transition: {
-			duration: 1,
-		},
-	},
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
 };
 
 const LoginPage = () => {
-	const { register, handleSubmit } = useForm({
-		resolver: yupResolver(loginSchema),
-	});
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
 
-	const { initController } = useUser();
-	const history = useHistory();
+  const { initController, user } = useUser();
+  const history = useHistory();
 
-	const login = async (data: ILoginData) => {
-		const controller = initController();
-		await controller.login(data);
-		history.push("/");
-	};
+  const login = async (data: ILoginData) => {
+    const controller = initController();
+    await controller.login(data);
+    if (user.auth) {
+      history.push("/");
+    }
+  };
 
-	return (
-		<Container variants={Motion} initial="hidden" animate="visible">
-			<Header>
-				<div className="container">
-					<div>
-						<h1>Bem vindo ao iRoça</h1>
-						<p>
-							Venda seus produtos ou compre de produtores agrícolas de todo o
-							Brasil
-						</p>
-					</div>
-					<LogoSVGdesktop />
-				</div>
-			</Header>
-			<form onSubmit={handleSubmit(login)}>
-				<div>
-					<h2>Entrar</h2>
-					<Input
-						type="email"
-						name="email"
-						register={register}
-						placeholder="Email"
-						data-testid="login"
-						color="green"
-					/>
-					<Input
-						type="password"
-						name="password"
-						register={register}
-						placeholder="Senha"
-						data-testid="password"
-						color="green"
-					/>
-					<Button color="green" type="submit">
-						Entrar
-					</Button>
-				</div>
-				<Link to="/register">
-					Ainda não possui uma conta ? Pode se registrar clicando aqui
-				</Link>
-			</form>
-		</Container>
-	);
+  return (
+    <Container variants={Motion} initial="hidden" animate="visible">
+      <Header>
+        <div className="container">
+          <div>
+            <h1>Bem vindo ao iRoça</h1>
+            <p>
+              Venda seus produtos ou compre de produtores agrícolas de todo o
+              Brasil
+            </p>
+          </div>
+          <LogoSVGdesktop />
+        </div>
+      </Header>
+      <form onSubmit={handleSubmit(login)}>
+        <div>
+          <h2>Entrar</h2>
+          <Input
+            type="email"
+            name="email"
+            register={register}
+            placeholder="Email"
+            data-testid="login"
+            color="green"
+          />
+          <Input
+            type="password"
+            name="password"
+            register={register}
+            placeholder="Senha"
+            data-testid="password"
+            color="green"
+          />
+          <Button color="green" type="submit">
+            Entrar
+          </Button>
+        </div>
+        <Link to="/register">
+          Ainda não possui uma conta ? Pode se registrar clicando aqui
+        </Link>
+      </form>
+    </Container>
+  );
 };
 
 export default LoginPage;
